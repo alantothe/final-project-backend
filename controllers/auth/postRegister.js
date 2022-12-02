0de7883106c769509b26494e718ef48fc1c2d2b4
alantothe <rubenamalpartida@gmail.com>
2022-12-02 01:10:51 -0500

    register user in mongoDB

--- controllers/auth/postRegister.js
+++ controllers/auth/postRegister.js
@@ -1,5 +1,48 @@
+const User = require('../../models/user');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken")
+
+
 const postRegister = async (req, res) => {
-    res.send("register route");
+    try{
+        const {username, email, password} =req.body
+        // check if user exists
+        const userExists = await User.exists({ email: email.toLowerCase() });
+        if (userExists) {
+        return res.status (409).send("E-mail already in use.");
+        }
+        // encrypt password
+        const encryptedPassword = await bcrypt.hash(password, 10);
+
+        // create user and save to mongoDB
+
+        const user = await User.create({
+            username,
+            email: email.toLowerCase(),
+            password: encryptedPassword
+        })
+
+        //create JWT token
+        const token = "";
+
+        res.status (201).json({
+            userDetails: {
+            email: user.email,
+            token: token,
+            username: user.username,
+            },
+        })
+
+
+
+
+
+
+
+    }
+    catch (err){
+        return res.status(500).send("Error, Please try again")
+    }
     };
 
     module.exports = postRegister
